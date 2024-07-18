e573aeec41cb5c0ffa75c93aed1e61bab9361863
A.Westwood <personal.email.aw1@gmail.com>
2024-07-18 13:30:20 +0100

    22. add params for lat, lon, timezone; remove those attributes from link

--- weather.js
+++ weather.js
@@ -1,3 +1,13 @@
-//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=Europe%2FLondon
+import axios from "axios"
 
-export function getWeather(lat, lon, timezone) {}
+//https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+    axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime", { 
+        params: {
+            latitude: lat,
+            longitude: lon,
+            timezone ,
+        },
+    })
+}
