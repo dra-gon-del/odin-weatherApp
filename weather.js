ed8b8efbe8d96100f945dc53fd7d347c7ec07efe
A.Westwood <personal.email.aw1@gmail.com>
2024-07-18 13:01:32 +0100

    21. get open api data on open-meteo

---
+++ weather.js
@@ -0,0 +1,3 @@
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=Europe%2FLondon
+
+export function getWeather(lat, lon, timezone) {}
