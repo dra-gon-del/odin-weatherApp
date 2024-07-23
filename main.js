738ab2ad8763f5a5f6151ecedd6a0e781c0c9ed9
A.Westwood <personal.email.aw1@gmail.com>
2024-07-23 12:29:20 +0100

    28. (main.js) change .then func, add renderWeather func

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import "./style.css";
 import { getWeather } from "./weather";
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    data => {
-        console.log(data)
-    }
-)
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).
+catch(e => {
+    console.error(e)
+    alert("Error getting weather.")
+})
+
+function renderWeather({ current, daily, hourly }) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+function renderCurrentWeather(current) {
+    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+}
