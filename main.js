c7bcd6a1ca6ca6bbaa0d2d056a75d04e6b4261d9
A.Westwood <personal.email.aw1@gmail.com>
2024-07-23 13:19:00 +0100

    29. (main.js) do js for current weather informations

--- main.js
+++ main.js
@@ -14,6 +14,22 @@ function renderWeather({ current, daily, hourly }) {
     document.body.classList.remove("blurred")
 }
 
-function renderCurrentWeather(current) {
-    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+function setValue(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
 }
+
+function getIconUrl(iconCode) {
+    return `icons/${iconCode}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+    }
